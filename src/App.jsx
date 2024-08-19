99d9c151d7d64e6e54023ca851914323be98aac0
Christopher Young <christopheryoung@MacBook123.local>
2024-08-19 11:25:49 -0700

    added socket

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import './App.scss'
+// Socket
+import io from "socket.io-client";
 // Components
 import MessageForm from "./components/forms/MessageForm";
 import MessagesDisplay from "./components/displays/MessagesDisplay";
+
+// Initialize socket
+const socket = io("http://localhost:5050");
+
+let submitMessage = (e, message) => {
+  e.preventDefault();
+  socket.emit("message", message);
+}
 
 function App() {
   return (
     <div>
       <MessagesDisplay/>
-      <MessageForm/>
+      <MessageForm submitMessage={submitMessage}/>
     </div>
   )
 }
--- src/components/forms/MessageForm.jsx
+++ src/components/forms/MessageForm.jsx
@@ -1,13 +1,21 @@
 import "./MessageForm.scss";
+// React
+import { useState } from "react";
 // Bootstrap
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
-const MessageForm = () => {
+const MessageForm = ({ submitMessage }) => {
+  // Controlled inputs
+  const [message, setMessage] = useState("");
+
   return (
-    <Form>
+    <Form onSubmit={e => submitMessage(e, message)}>
       <Form.Group>
-        <Form.Control type="text" placeholder="Message" />
+        <Form.Control 
+          onChange={e => setMessage(e.target.value)}
+          type="text"
+          placeholder="Message" />
       </Form.Group>
 
       <Button variant="primary" type="submit">
